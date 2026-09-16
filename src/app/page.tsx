38d31de827602277b8d6e7e8dadc825b8e3d89de
Sumolegen <dayasumo123@gmail.com>
2026-09-16 16:31:06 +0530

    CTAPreview-added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import ExpertisePreview from "@/Components/sections/ExpertisePreview";
 import WhyDayaPreview from "../Components/sections/WhyDayaPreview";
 import JourneyPreview from "../Components/sections/JourneyPreview";
 import RecognitionPreview from "../Components/sections/RecognitionPreview";
+import BlogPreview from "../Components/sections/BlogPreview";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <WhyDayaPreview />
       <JourneyPreview />
       <RecognitionPreview />
+      <BlogPreview />
     </main>
   );
 }
